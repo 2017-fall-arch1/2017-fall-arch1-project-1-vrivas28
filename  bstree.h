76b6007d2b07d778a0eff499e24b0ef4f489ede6
student <student@localhost.localdomain>
2017-10-16 10:11:50 -0600

    Added new methods.

---
+++  bstree.h
@@ -0,0 +1,9 @@
+#ifndef bstree_included
+#define bstree_included
+
+typedef struct BNode{
+  struct BNode *right, *left;
+  char *value;
+}
+  
+  #endif 
---
+++ bstree.h
@@ -0,0 +1,29 @@
+#ifndef bstree_included
+#define bstree_included
+
+typedef struct BNode{
+  struct BNode *right, *left;
+  char *value;
+}BNode;
+
+BNode *bsAlloc();
+/* add new name to the tree*/
+
+void addNode(BNode *nb, char *name);
+
+/* removes a name from tree*/
+
+void removeNode(BNode *nb, char *name);
+
+/* Prints the values of the tree in order*/
+
+void printTree(BNode *root);
+
+void writeInFile(char *namw);
+
+void readFile();
+
+
+#endif
+
+  
