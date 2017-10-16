87c4e889aaeacb8d8193678c328194b81158e35e
student <student@localhost.localdomain>
2017-10-16 14:51:07 -0600

    Missing remove name from tree

--- bstree.c
+++ bstree.c
@@ -26,4 +26,47 @@ BNode addNode(BNode *node, char *eName)
   }
   return node;
 }
-
+void removeNode(BNode *node, char *eName)
+{
+  if(node == NULL)
+    return node;
+  if(strncmp(node->name, eName) < 0){
+  }
+}
+/* Will open the file and extract each name and add it to the tree.*/
+BNode readFile(BNode *node, char *fileName)
+{
+  FILE *fp;
+  fp = fopen(fileName,"r");
+  int line  = getc(fp);
+  char *name = (char *)malloc(sizeof(char));
+  while(line != EOF)
+    {
+      fscanf(fp, %s, name);
+      node = addNode(node,name);
+      line = getc(fp);
+    }
+  fclose(fp);
+  return node;
+}
+void writeToFile(BNode *node, char *fileName)
+{
+  FILE *fp;
+  fp = fopen(fileName, "w");
+  if(fp == NULL){
+    print("File does not Exist!!");
+    return;}
+  if(node == NULL)
+    return;
+  writeToFile(node->left,fileName);
+  fprintf(fp,%s\n,node->name);
+  writeToFile(node->right,fileName);;
+}
+void printTree(BNode *node)
+{
+  if(node == NULL)
+    return;
+  printTree(node->left);
+  printf(%s\n,node->name);
+  printTree(node->right);
+}
