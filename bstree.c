819a71a1a9faa21a5e5d418d419d2de27fa1e863
student <student@localhost.localdomain>
2017-10-16 18:04:42 -0600

    Main method added

--- bstree.c
+++ bstree.c
@@ -60,7 +60,7 @@ void writeToFile(BNode *node, char *fileName)
     return;
   writeToFile(node->left,fileName);
   fprintf(fp,%s\n,node->name);
-  writeToFile(node->right,fileName);;
+  writeToFile(node->right,fileName);
 }
 void printTree(BNode *node)
 {
@@ -70,3 +70,59 @@ void printTree(BNode *node)
   printf(%s\n,node->name);
   printTree(node->right);
 }
+main()
+{
+  BNode *node;
+  node = NULL;
+  int cont = 1;
+  int command = 0;
+  char *name;
+  printf("Welcome to the Personnel Management System!\n");
+
+  while(cont == 1)
+    {
+      prinf("Please enter one of these commands.\n (1) Add new employee.\n"
+	    "(2) Remove employee.\n (3) Display list of employees.\n"
+	    "(4) Add employees from file.\n"
+	    "(5) Write employees' names to file.\n (6) EXIT \n");
+      scanf("%d",command);
+      if(command == 1)
+	{
+	  printf("Please Enter employee's name.\n");
+	  scanf("%s",name);
+	  addNode(node,name);
+	}
+      else if(command == 2)
+	{
+	  printf("Please enter the employee's name that you wish to delete.\n");
+	  scanf("%s",name);
+	  removeNode(node,name);
+	}
+      else if(command == 3)
+	{
+	  printTree(node);
+	}
+      else if(command == 4)
+	{
+	  printf("Please enter name of File.\n");
+	  scanf("%s",name);
+	  readFile(node,name);
+	}
+      else if(command == 5)
+	{
+	  printf("Please enter name of File.\n");
+	  scanf("%s",name);
+	  writeToFile(node, name);
+	}
+      else if(command == 6)
+	{
+	  cont = 0;
+	  return;
+	}
+      else
+	printf("Not a valid command. Please try again.\n");
+    }
+  printf("BYE!!\n");
+}
+
+	
